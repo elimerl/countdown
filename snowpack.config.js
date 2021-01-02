9ab30d1141915d286628770f98fc258193d15447
elimerl <elimerlindavies@gmail.com>
2021-01-01 18:11:10 -0800

    oop

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,13 +1,13 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
-    public: {url: '/', static: true},
-    src: {url: '/dist'},
+    public: { url: "/", static: true },
+    src: { url: "/dist" },
   },
   plugins: [
-    '@snowpack/plugin-svelte',
-    '@snowpack/plugin-dotenv',
-    '@snowpack/plugin-typescript',
+    "@snowpack/plugin-svelte",
+    "@snowpack/plugin-dotenv",
+    "@snowpack/plugin-typescript",
   ],
   install: [
     /* ... */
